9c79ced9f9194afbdd9db1cc44c49f2ec28c4e0e
Kosuke Matsuda <matsuda@MBPr.local>
2015-05-05 01:36:45 +0900

    create test project for geocoder

---
+++ TestGeocoder/TestGeocoder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGeocoder
+//
+//  Created by Kosuke Matsuda on 2015/05/05.
+//  Copyright (c) 2015年 Kosuke Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
