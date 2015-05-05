2417cc3d1f97c360a87f5183387e564c78712a10
Kosuke Matsuda <matsuda@MBPr.local>
2015-05-05 17:17:59 +0900

    add geocoding

--- TestGeocoder/TestGeocoder/ViewController.swift
+++ TestGeocoder/TestGeocoder/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    lazy var locationManager: CLLocationManager! = self.setupLocationManager()
+//    lazy var locationManager: CLLocationManager! = {
+//        let manager = CLLocationManager()
+//        manager.delegate = self
+//        return manager
+//    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        APPLog()
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapGeocoderButton(sender: AnyObject) {
+        let address = "渋谷区"
+        var geocoder = CLGeocoder()
+        geocoder .geocodeAddressString(address, completionHandler: { (placemarks, error) -> Void in
+            if error != nil {
+                APPLog("\(error)")
+            } else {
+                APPLog("\(placemarks.count)")
+                if let place = placemarks.first as? CLPlacemark {
+                    APPLog("\(place)")
+                    APPLog("\(place.name)")
+                    let coordinate = place.location.coordinate
+                    APPLog("latitude : \(coordinate.latitude)")
+                    APPLog("longitude : \(coordinate.longitude)")
+                }
+            }
+        })
+    }
 
+    func setupLocationManager() -> CLLocationManager {
+        APPLog()
+        let manager = CLLocationManager()
+        manager.delegate = self
+        return manager
+    }
+
+    // MARK: location manager delegate
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        APPLog("location : \(location)")
+        APPLog("latitude : \(location.coordinate.latitude)")
+        APPLog("longitude : \(location.coordinate.longitude)")
+    }
+
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        APPLog("error : \(error)")
+    }
 }
 
