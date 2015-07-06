ba5de8ba50481eacde3753ff91137406aa713eb5
matsuda <kmatsuda@appirits.com>
2015-07-06 15:09:08 +0900

    create project for UIVisualEffectView

---
+++ TestVisualEffect/TestVisualEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestVisualEffect
+//
+//  Created by Kosuke Matsuda on 2015/07/06.
+//  Copyright © 2015年 matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
