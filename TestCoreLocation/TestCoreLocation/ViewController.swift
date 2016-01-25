56a1a82268f4d3f70d3d80176afaf83c919aefed
matsuda <kosukematsuda@gmail.com>
2016-01-25 10:36:20 +0900

    create project

---
+++ TestCoreLocation/TestCoreLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCoreLocation
+//
+//  Created by Kosuke Matsuda on 2016/01/25.
+//  Copyright © 2016年 Kosuke Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
